e9b891e03c0b18fd0bfdeda29b19b51fb225c098
Will Feng <willfeng@fb.com>
2018-04-18 17:27:26 -0400

    Clean up manager_pid

--- torch/utils/data/dataloader.py
+++ torch/utils/data/dataloader.py
@@ -52,7 +52,7 @@ if IS_WINDOWS:
 
             # Value obtained from https://msdn.microsoft.com/en-us/library/ms684880.aspx
             SYNCHRONIZE = 0x00100000
-            self.manager_handle = kernel32.OpenProcess(SYNCHRONIZE, 0, manager_pid)
+            self.manager_handle = kernel32.OpenProcess(SYNCHRONIZE, 0, self.manager_pid)
 
             if not self.manager_handle:
                 raise ctypes.WinError(ctypes.get_last_error())
@@ -69,7 +69,7 @@ else:
             return os.getppid() == self.manager_pid
 
 
-def _worker_loop(dataset, index_queue, data_queue, collate_fn, seed, init_fn, worker_id, manager_pid):
+def _worker_loop(dataset, index_queue, data_queue, collate_fn, seed, init_fn, worker_id):
     global _use_shared_memory
     _use_shared_memory = True
 
@@ -255,13 +255,12 @@ class _DataLoaderIter(object):
             self.reorder_dict = {}
 
             base_seed = torch.LongTensor(1).random_()[0]
-            manager_pid = os.getpid()
             self.workers = [
                 multiprocessing.Process(
                     target=_worker_loop,
                     args=(self.dataset, self.index_queues[i],
                           self.worker_result_queue, self.collate_fn, base_seed + i,
-                          self.worker_init_fn, i, manager_pid))
+                          self.worker_init_fn, i))
                 for i in range(self.num_workers)]
 
             if self.pin_memory or self.timeout > 0:
