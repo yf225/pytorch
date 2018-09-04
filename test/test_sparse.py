244cb767ea08c2646ef86857d90974b909476598
Will Feng <willfeng@fb.com>
2018-09-03 23:41:35 -0400

    Add empty tensor test to test_factory_copy

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -1311,21 +1311,37 @@ class TestSparse(TestCase):
         indices = torch.tensor(([0], [2]), dtype=torch.int64)
         values = torch.tensor([1.], dtype=torch.float64)
         test_tensor(indices, values, True, True)
+
+        indices = torch.tensor(([0], [2]), dtype=torch.int64)
+        values = torch.DoubleTensor(1, 0)
+        test_tensor(indices, values, True, True)
         
         # only indices correct
         indices = torch.tensor(([0], [2]), dtype=torch.int64)
         values = torch.tensor([1.], dtype=torch.float32)
         test_tensor(indices, values, True, False)
+
+        indices = torch.tensor(([0], [2]), dtype=torch.int64)
+        values = torch.FloatTensor(1, 0)
+        test_tensor(indices, values, True, True)  # An empty tensor's data_ptr is always equal to 0
         
         # only values correct
         indices = torch.tensor(([0], [2]), dtype=torch.int32)
         values = torch.tensor([1.], dtype=torch.float64)
+        test_tensor(indices, values, False, True)
+
+        indices = torch.tensor(([0], [2]), dtype=torch.int32)
+        values = torch.DoubleTensor(1, 0)
         test_tensor(indices, values, False, True)
         
         # neither correct
         indices = torch.tensor(([0], [2]), dtype=torch.int32)
         values = torch.tensor([1.], dtype=torch.float32)
         test_tensor(indices, values, False, False)
+
+        indices = torch.tensor(([0], [2]), dtype=torch.int32)
+        values = torch.FloatTensor(1, 0)
+        test_tensor(indices, values, False, True)  # An empty tensor's data_ptr is always equal to 0
 
     @cpu_only  # not really, but we only really want to run this once
     def test_dtypes(self):
