7bdb763d5c76cfed6591e2c35e66df29887fd23a
Will Feng <willfeng@fb.com>
2018-09-03 22:43:20 -0400

    Add empty tensor test to test_factory

--- test/test_sparse.py
+++ test/test_sparse.py
@@ -1117,32 +1117,43 @@ class TestSparse(TestCase):
     @cpu_only  # not really, but we only really want to run this once
     @skipIfRocm
     def test_factory(self):
-        default_size = torch.Size([1, 3])
-        size = torch.Size([3, 3])
-        for include_size in [True, False]:
-            for use_tensor_idx in [True, False]:
-                for use_tensor_val in [True, False]:
-                    for use_cuda in ([False] if not torch.cuda.is_available() else [True, False]):
-                        # have to include size with cuda sparse tensors
-                        include_size = include_size or use_cuda
-                        dtype = torch.float64
-                        long_dtype = torch.int64
-                        device = torch.device('cpu') if not use_cuda else torch.device(torch.cuda.device_count() - 1)
-                        indices = torch.tensor(([0], [2]), dtype=long_dtype) if use_tensor_idx else ([0], [2])
-                        values = torch.tensor([1.], dtype=dtype) if use_tensor_val else 1.
-                        if include_size:
-                            sparse_tensor = torch.sparse_coo_tensor(indices, values, size, dtype=dtype,
-                                                                    device=device, requires_grad=True)
-                        else:
-                            sparse_tensor = torch.sparse_coo_tensor(indices, values, dtype=dtype,
-                                                                    device=device, requires_grad=True)
-                        self.assertEqual(indices, sparse_tensor._indices())
-                        self.assertEqual(values, sparse_tensor._values())
-                        self.assertEqual(size if include_size else default_size, sparse_tensor.size())
-                        self.assertEqual(dtype, sparse_tensor.dtype)
-                        if use_cuda:
-                            self.assertEqual(device, sparse_tensor._values().device)
-                        self.assertEqual(True, sparse_tensor.requires_grad)
+        for test_empty_tensor in [True, False]:
+            if test_empty_tensor:
+                default_size = torch.Size([1, 3, 0])
+                size = torch.Size([3, 3, 0])
+            else:
+                default_size = torch.Size([1, 3])
+                size = torch.Size([3, 3])
+            for include_size in [True, False]:
+                for use_tensor_idx in [True, False]:
+                    for use_tensor_val in [True, False]:
+                        for use_cuda in ([False] if not torch.cuda.is_available() else [True, False]):
+                            # have to include size with cuda sparse tensors
+                            include_size = include_size or use_cuda
+                            dtype = torch.float64
+                            long_dtype = torch.int64
+                            device = torch.device('cpu') if not use_cuda else torch.device(torch.cuda.device_count() - 1)
+                            indices = torch.tensor(([0], [2]), dtype=long_dtype) if use_tensor_idx else ([0], [2])
+                            if test_empty_tensor:
+                                values = self.ValueTensor(1, 0)
+                            else:
+                                if use_tensor_val:
+                                    values = torch.tensor([1.], dtype=dtype)
+                                else:
+                                    values = 1.
+                            if include_size:
+                                sparse_tensor = torch.sparse_coo_tensor(indices, values, size, dtype=dtype,
+                                                                        device=device, requires_grad=True)
+                            else:
+                                sparse_tensor = torch.sparse_coo_tensor(indices, values, dtype=dtype,
+                                                                        device=device, requires_grad=True)
+                            self.assertEqual(indices, sparse_tensor._indices())
+                            self.assertEqual(values, sparse_tensor._values())
+                            self.assertEqual(size if include_size else default_size, sparse_tensor.size())
+                            self.assertEqual(dtype, sparse_tensor.dtype)
+                            if use_cuda:
+                                self.assertEqual(device, sparse_tensor._values().device)
+                            self.assertEqual(True, sparse_tensor.requires_grad)
 
     @skipIfRocm
     def test_factory_size_check(self):
@@ -1441,7 +1452,7 @@ def load_tests(loader, tests, pattern):
         test_suite = unittest.TestSuite()
         for test_group in tests:
             for test in test_group:
-                if 'test_new' in str(test):
+                if 'test_factory' in str(test):
                     test_suite.addTest(test)
         return test_suite
 
