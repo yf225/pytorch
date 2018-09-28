b25afbca3de1c28d7d3f33ae787b3dfe3d21cfe4
Will Feng <willfeng@fb.com>
2018-09-27 20:03:05 -0400

    try to fix sparse storage bug

--- aten/src/ATen/core/TensorImpl.cpp
+++ aten/src/ATen/core/TensorImpl.cpp
@@ -31,16 +31,16 @@ TensorImpl::TensorImpl(Storage&& storage, TensorTypeId type_id, const caffe2::Ty
       is_variable_(is_variable) {}
 
 TensorImpl::TensorImpl(const TensorImpl& tensor_impl)
-    : storage_(tensor_impl.storage()),
-      storage_offset_(tensor_impl.storage_offset()),
-      sizes_(tensor_impl.sizes().begin(), tensor_impl.sizes().end()),
-      strides_(tensor_impl.strides().begin(), tensor_impl.strides().end()),
-      is_contiguous_(tensor_impl.is_contiguous()),
-      numel_(tensor_impl.numel()),
-      type_id_(tensor_impl.type_id()),
-      data_type_(tensor_impl.dtype()),
-      is_wrapped_number_(tensor_impl.is_wrapped_number()),
-      is_variable_(tensor_impl.is_variable()),
+    : storage_(tensor_impl.storage_),
+      storage_offset_(tensor_impl.storage_offset_),
+      sizes_(tensor_impl.sizes_),
+      strides_(tensor_impl.strides_),
+      is_contiguous_(tensor_impl.is_contiguous_),
+      numel_(tensor_impl.numel_),
+      type_id_(tensor_impl.type_id_),
+      data_type_(tensor_impl.data_type_),
+      is_wrapped_number_(tensor_impl.is_wrapped_number_),
+      is_variable_(tensor_impl.is_variable_),
       variable_impl_(tensor_impl.get_variable_impl()) {}
 
 IntList TensorImpl::sizes() const {
