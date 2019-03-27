56f1510dd3785ffe0c4215c92c29c71109396c10
Will Feng <willfeng@fb.com>
2019-03-27 11:38:56 -0400

    test

--- aten/src/ATen/test/cuda_packedtensoraccessor_test.cu
+++ aten/src/ATen/test/cuda_packedtensoraccessor_test.cu
@@ -21,6 +21,18 @@ __global__ void test_tensor_packed_accessor_kernel(
   }
 }
 
+__global__ void test_std_vector_tensor(
+    std::vector<torch::Tensor> v_t1a) {
+  double total = 0;
+  for (int64_t i = 0; i < v_t1a.size(); i++) {
+    auto accessor = v_t1a[i];
+    for (int64_t j = 0; j < accessor.size(0); j++) {
+      total += accessor[j];
+    }
+  }
+  v_t1a[0][0] = total;
+}
+
 // test PackedTensorAccessor and Tensor.packed_accessor
 TEST(PackedtensoraccessorTest, PackedtensoraccessorTestCUDA) {
   if (!at::cuda::is_available()) return;
@@ -44,4 +56,21 @@ TEST(PackedtensoraccessorTest, PackedtensoraccessorTestCUDA) {
   auto expected = mv(t1, t2);
 
   ASSERT_TRUE(res.allclose(expected));
+}
+
+TEST(PackedtensoraccessorTest, PackedtensoraccessorTestStdVector) {
+  if (!at::cuda::is_available()) throw 42;
+
+  Tensor t1 = empty({4}, CUDA(kFloat));
+
+  // auto t1a = t1.packed_accessor<float, 1, RestrictPtrTraits>();
+
+  std::vector<torch::Tensor> vec = {t1};
+
+  auto stream = at::cuda::getCurrentCUDAStream();
+
+  test_tensor_packed_accessor_kernel<<<1, 1, 0, stream>>>(vec);
+  cudaError_t err = cudaDeviceSynchronize();
+  bool isEQ = err == cudaSuccess;
+  ASSERT_TRUE(isEQ);
 }
